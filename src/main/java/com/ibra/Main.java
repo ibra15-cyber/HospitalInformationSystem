4d974a8fa35ffa53bfaccfe62f220436bf4bdf5c
unknown <iasafianu@st.ug.edu.gh>
2025-05-08 17:58:56 +0000

    chore: refactoring

--- src/main/java/com/ibra/Main.java
+++ src/main/java/com/ibra/Main.java
@@ -11,24 +11,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         try {
-                PatientService patientService = new PatientServiceImp();
-
-                Patient newPatient = new Patient();
-                newPatient.setFirstName("John");
-                newPatient.setSurname("Doe");
-                newPatient.setAddress("Accra New Town");
-                newPatient.setTelephone("555-123-4567");
-
-                Patient patient = patientService.addPatient(newPatient);
-
-                System.out.println(patient);
-
-                // Get all patients
-                List<Patient> allPatients = patientService.getAllPatients();
-                for (Patient p : allPatients) {
-                    System.out.println(p.getPatientId()+ " " + p.getFirstName() + " " + p.getSurname());
-                }
-
                 HospitalAppUI.main(args);
         } finally {
             DBConnection.closeConnection();
